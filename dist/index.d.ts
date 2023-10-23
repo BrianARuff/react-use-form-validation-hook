4b0c874b3ffe69987a4460b931201795883dc045
Brian Ruff <briananthonyruff@gmail.com>
2023-10-23 17:39:21 -0400

    npm version updates and example usage update in docs for npm, npm (minor) version bump

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,15 @@
+/**
+ * Defines the validation rules for a form data object.
+ */
 export declare type ValidationRules<T> = {
     [K in keyof T]: (value: T[K]) => true | string;
 };
+/**
+ * A custom hook that provides form validation functionality.
+ * @template T - The type of the form data object.
+ * @param {T} initialState - The initial state of the form data object.
+ * @param {ValidationRules<T>} validationRules - The validation rules for the form data object.
+ * @returns {[T, Partial<T>]} - A tuple containing the proxied form data object and the validation errors object.
+ * @example
+**/
 export declare const useFormValidation: <T extends Record<string, any>>(initialState: T, validationRules: ValidationRules<T>) => [T, Partial<T>];
